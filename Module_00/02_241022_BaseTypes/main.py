6a76b1a81e5cb74f9bb78716031d5e4b6f0953b4
pikalovmusic <166620147+pikalovmusic@users.noreply.github.com>
2024-10-22 21:34:15 +0300

    -

---
+++ Module_00/02_241022_BaseTypes/main.py
@@ -0,0 +1,17 @@
+# 1st program
+print(9**0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3nd program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a)
+print(b)
+print(a == b)
+
+# 4th program
+x = "123.456"
+x = (float(x) * 10) % 10
+print(int(x))
