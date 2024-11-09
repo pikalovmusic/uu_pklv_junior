faa4a4bbd7c15347f83e960d8d8fa3d51c273dda
pikalovmusic <166620147+pikalovmusic@users.noreply.github.com>
2024-11-09 13:30:56 +0300

    Add: module_3_1

---
+++ Module_03/01_241109_NameSpace/module_3_1.py
@@ -0,0 +1,28 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string: str) -> tuple:
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+
+def is_contains(string: str, list_to_search: list) -> bool:
+    count_calls()
+    string_to_search = ' '.join(list_to_search).lower()
+
+    match string_to_search.__contains__(string.lower()):
+        case True: return True
+        case Default: return False
+
+
+# Пример из урока (для проверки себя):
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
