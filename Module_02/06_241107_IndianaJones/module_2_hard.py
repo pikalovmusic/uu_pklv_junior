50f6cfb64acc9bfea557005e14ea9e5e59d0b09b
pikalovmusic <166620147+pikalovmusic@users.noreply.github.com>
2024-11-09 20:47:32 +0300

    Mod module_2_hard.py

--- Module_02/06_241107_IndianaJones/module_2_hard.py
+++ Module_02/06_241107_IndianaJones/module_2_hard.py
@@ -1,15 +1,17 @@
 def get_password(num: int):
     result = ''
-    for a in range(1, num):
-        for b in range(a + 1, num):
-            if num % (a + b) == 0 or a + b == num: result += '%s%s' % (str(a), str(b))
+    for symbol_1 in range(1, num):
+        for symbol_2 in range(symbol_1 + 1, num):
+            if num % (symbol_1 + symbol_2) == 0 or symbol_1 + symbol_2 == num:
+                result += '%s%s' % (str(symbol_1), str(symbol_2))
 
     return result
 
 
 while True:
     user_input = input('Введите число из первого поля: ')
-    if user_input.lower() == 'exit': exit(0)
+    if user_input.lower() == 'exit':
+        exit(0)
 
     print(f'Пароль: {get_password(int(user_input))}\n')
 
