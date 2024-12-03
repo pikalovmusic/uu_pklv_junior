2cf385ac5377b95b3a7b542fc1d4e6c249e739b6
pikalovmusic <166620147+pikalovmusic@users.noreply.github.com>
2024-12-03 13:16:38 +0300

    add task MagicTasks

---
+++ Module_05/02_241203_MagicTasks/main.py
@@ -0,0 +1,30 @@
+class House():
+    def __init__(self, name: str, floors: int):
+        self.name = name
+        self.number_of_floors = floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def go_to(self, new_floor: int):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+            return
+        for floor in range(1, new_floor+1):
+            print(floor)
+
+
+# Для проверки:
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
