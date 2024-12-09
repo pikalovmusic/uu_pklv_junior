f09afb58e89f3758294039dd87cd7ff87c6087db
pikalovmusic <166620147+pikalovmusic@users.noreply.github.com>
2024-12-10 00:33:19 +0300

    mod task UrTube

--- Module_05/06_241209_UrTube/urtube.py
+++ Module_05/06_241209_UrTube/urtube.py
@@ -11,6 +11,11 @@ class User():
     def __repr__(self):
         return f'{self.nickname}, {self.age}y.o.'
 
+    def password_check(self, password: str) -> bool:
+        if hash(password) == self.password:
+            return True
+        return False
+
 
 class Video():
     def __init__(self, title: str, duration: int, adult_mode=False):
@@ -22,15 +27,25 @@ class Video():
         return self.title
 
     def __repr__(self) -> str:
-        return self.__str__()
+        # в примере вывода зачем-то были ковычки, добавил их сюда
+        return f"'{self.__str__()}'"
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Video):
+            return self.title.lower() == other.title.lower()
+        elif isinstance(other, str):
+            return self.title.lower() == other.lower()
+        return False
 
     def playing_process(self):
+        print(f'''\n:: ТУТ ПОТОМ БУДЕТ ВИДЖЕТ ПЛЕЕРА\n:: NOW PLAYING: {
+              self.title}''')
         for sec in range(self.time_now+1, self.duration+1):
-            print(sec, end=' ', flush=True)
             sleep(1)
+            print(sec, end=' ', flush=True)
             self.time_now = sec
         self.time_now = 0
-        print('Конец видео')
+        print('Конец видео\n')
 
 
 class UrTube():
@@ -42,9 +57,9 @@ class UrTube():
             if user.nickname.lower() == nickname.lower():
                 return user
 
-    def get_video(self, title: str) -> Video:
+    def get_video(self, request) -> Video:
         for video in self.videos:
-            if video.title.lower() == title.lower():
+            if video == request:
                 return video
 
     def get_videos(self, request: str) -> list:
@@ -57,7 +72,7 @@ class UrTube():
     def log_in(self, nickname: str, password: str):
         user = self.get_user(nickname)
         if user != None:
-            if user.password == hash(password):
+            if user.password_check(password):
                 self.current_user = user
                 return
 
@@ -70,14 +85,14 @@ class UrTube():
 
         new_user = User(nickname.lower(), hash(password), age)
         self.users.append(new_user)
-        self.current_user = self.users[-1]
+        self.log_in(nickname, password)
 
     def log_out(self):
         self.current_user = None
 
     def add(self, *videos: Video):
         for video in videos:
-            if self.get_video(video.title) == None:
+            if self.get_video(video) == None:
                 self.videos.append(video)
 
     def watch_video(self, title: str):
@@ -85,10 +100,9 @@ class UrTube():
             print('Войдите в аккаунт, чтобы смотреть видео')
             return
 
-        for video in self.videos:
-            if video.title.lower() == title.lower():
-                if video.adult_mode and self.current_user.age < 18:
-                    print('Вам нет 18 лет, пожалуйста покиньте страницу')
-                    return
-                video.playing_process()
-                break
+        video = self.get_video(title)
+        if video != None:
+            if video.adult_mode and self.current_user.age < 18:
+                print('Вам нет 18 лет, пожалуйста покиньте страницу')
+                return
+            video.playing_process()
