e4c8ae6559c2952000b0414e7e76f5278d11c990
pikalovmusic <166620147+pikalovmusic@users.noreply.github.com>
2025-01-09 00:24:42 +0300

    mod task Decorator

--- Module_09/07_250108_Decorator/main.py
+++ Module_09/07_250108_Decorator/main.py
@@ -5,16 +5,16 @@
 
 
 def is_prime(func):
-    def prime_or_not(*args) -> int:
-        num, num_type = func(*args), 'Простое'
-        if num != 1:  # единица - не простое и не составное число. Ничего не принтим
+    def wrapper(*args, **kwargs) -> int:
+        num, num_type = func(*args, **kwargs), 'Простое'
+        if num > 1:  # отрицательные, ноль и единица - ничего не принтим
             for i in range(2, num):
                 if num % i == 0:
                     num_type = 'Составное'
                     break
             print(num_type)
         return num
-    return prime_or_not
+    return wrapper
 
 
 @is_prime
@@ -24,4 +24,5 @@ def sum_three(a: int, b: int, c: int) -> int:
 
 print(sum_three(2, 3, 6), end='\n\n')
 print(sum_three(1, 1, 2), end='\n\n')
+print(sum_three(5, -15, 4), end='\n\n')
 print(sum_three(10, -12, 3))
